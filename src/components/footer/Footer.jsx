779e8c02b742a3e1b8cf2c29cdaaf22a42161580
AbdulazizObidov <abdulazizobidov03@gmail.com>
2023-04-05 12:14:06 +0500

    png

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import Logo from "../images/png";
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+   return (
+      <div>
+         <nav className="bg-black">
+            <div className="mx-auto px-4">
+               <div className="flex justify-between">
+                  <div className="flex items-center">
+                     <img
+                        src="{Logo }"
+                        alt="Netflix Logo"
+                        className="h-10"
+                     />
+                  </div>
+                  <div className="flex items-center">
+                     <a
+                        href="#"
+                        className="text-white hover:text-gray-300 text-sm mr-4"
+                     >
+                        Home
+                     </a>
+                     <a
+                        href="#"
+                        className="text-white hover:text-gray-300 text-sm mr-4"
+                     >
+                        TV Shows
+                     </a>
+                     <a
+                        href="#"
+                        className="text-white hover:text-gray-300 text-sm mr-4"
+                     >
+                        Movies
+                     </a>
+                     <a
+                        href="#"
+                        className="text-white hover:text-gray-300 text-sm mr-4"
+                     >
+                        New & Popular
+                     </a>
+                     <a
+                        href="#"
+                        className="text-white hover:text-gray-300 text-sm mr-4"
+                     >
+                        My List
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </nav>
+      </div>
+   );
 }
 
-export default Footer
+export default Footer;
