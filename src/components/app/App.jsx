136244a2ecbdc1e14fa59f96ba35abda4eb1fe76
AbdulazizObidov <abdulazizobidov03@gmail.com>
2023-04-05 11:58:45 +0500

    import

---
+++ src/components/app/App.jsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import React from "react";
+import Footer from "../footer/Footer";
+import Form from "../form/Form";
+import Header from "../haeder/Header";
+import Main from "../section/Main";
+
+const App = () => {
+   return (
+      <div>
+         <Footer />
+         <Form />
+         <Header />
+         <Main />
+      </div>
+   );
+};
+
+export default App;
